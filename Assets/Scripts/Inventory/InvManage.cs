551bad478f0e3796c8d90672f4ab2add418528bf
Sir Code <taylorschneider07@yahoo.com>
2020-07-28 22:04:51 -0400

    Physics and GUI changes.

---
+++ Assets/Scripts/Inventory/InvManage.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvManage : MonoBehaviour {
+    public List<ItemDataList> itemDataList;
+}
---
+++ Assets/Scripts/Inventory/ItemDataList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ItemDataList 
+{
+    public GameObject itemSlotCount;
+    public int itemId;
+    public int itemCount;
+}

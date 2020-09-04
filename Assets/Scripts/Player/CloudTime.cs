ec62e95ec182fa2c5abc77a68fd72d05b5607780
Sir Code <taylorschneider07@yahoo.com>
2020-09-03 22:20:40 -0400

    Completely redid skies and world

---
+++ Assets/Scripts/Player/CloudTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudTime : MonoBehaviour
+{
+    void Start() {
+        ParticleSystem ptcs = gameObject.GetComponent<ParticleSystem>();
+        ptcs.Simulate(500);
+        ptcs.Play();
+    }
+}
